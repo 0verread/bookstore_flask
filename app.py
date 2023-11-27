6b80f3e863acbf8e199fb716a00a037506c39ece
Jeet <97605913+0verread@users.noreply.github.com>
2023-11-27 18:44:13 -0500

    week2 initialize (#1)

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
+from src.models import BooksModel
+from src import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstoredb.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.app_context().push()
 
-# 127.0.0.1:8080/hello
-
-
-@app.route('/hello', methods=['GET'])
-def hello():
-	return "Hello world!"
-
-
-books_data = [
-	{"id": 1, "name": "War and Peace"},
-	{"id": 2, "name": "Crime and Punishment"},
-	{"id": 3, "name": "Anna Karenina"}
-]
-
+db.init_app(app)
+db.create_all()
 
 @app.route('/books', methods=['GET'])
-def get_books():
-	return jsonify(books_data)
+def get_all_books():
+	try:
+		books = BooksModel.query.all()
+		return make_response(jsonify([book.serialize for book in books]), 200)
+	except Exception as e:
+		return make_response(jsonify({'message': 'error getting Books', 'error': e}), 500)
+
+
+if __name__ == '__main__':
+	app.run(DEBUG=True)
 
 
---
+++ src/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from . import db
+
+class BooksModel(db.Model):
+	"""
+	Defines the Books model
+	"""
+	__tablename__ = "books"
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(80), nullable=False)
+	author = db.Column(db.String(225), nullable=False)
+	quantity = db.Column(db.Integer)
+
+	def __init__(self, id, title, author, quantity):
+		self.id = id
+		self.title = title
+		self.author = author
+		self.quantity = quantity
+
+	@property
+	def serialize(self):
+		return {"title": self.title, "author": self.author, "quantity": self.quantity}
+	
